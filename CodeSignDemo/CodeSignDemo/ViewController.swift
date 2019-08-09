5c8dc506d6f040099a1269fd5a12c92e6ad359f5
Shagun Madhikarmi <madhikarma@gmail.com>
2019-08-09 19:25:48 +0100

    Adds code signing demo

---
+++ CodeSignDemo/CodeSignDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeSignDemo
+//
+//  Created by Shagun Madhikarmi on 19/08/2017.
+//  Copyright © 2017 Shagun Madhikarmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
