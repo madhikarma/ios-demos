f1c8dbb7f8e50871b4a75d422ee4fa886cf1557d
Shagun Madhikarmi <madhikarma@gmail.com>
2019-11-07 02:02:48 +0000

    Catalyst: Adds normal iPhone / iPad app project

---
+++ mac-catalyst/CatalystDemo3/CatalystDemo3Tests/CatalystDemo3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CatalystDemo3Tests.swift
+//  CatalystDemo3Tests
+//
+//  Created by Beta on 07/11/2019.
+//  Copyright © 2019 Beta. All rights reserved.
+//
+
+import XCTest
+@testable import CatalystDemo3
+
+class CatalystDemo3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
