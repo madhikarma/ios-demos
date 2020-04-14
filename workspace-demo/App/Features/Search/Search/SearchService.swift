60672ad2d96c660c47e3f4d5f5a838f5cdcecd8c
Shagun Madhikarmi <madhikarma@gmail.com>
2020-04-15 00:29:30 +0100

    Workspaces: Adds SwiftPM based dependency for Search

--- workspace-demo/App/Features/Search/Search/SearchService.swift
+++ workspace-demo/App/Features/Search/Search/SearchService.swift
@@ -8,7 +8,7 @@
 
 import Combine
 import Foundation
-import NetworkClient
+import GitHubAPI
 
 public struct SearchResponse: Codable {
     public let total_count: UInt
@@ -19,18 +19,17 @@ public protocol SearchServiceProtocol {
 }
 
 public final class SearchService: SearchServiceProtocol {
-    private let network: NetworkClientProtocol
 
-    public init(_ network: NetworkClientProtocol = NetworkClient()) {
-        self.network = network
+    private let gitHubAPI: GitHubAPI
+    
+    public init(gitHubAPI: GitHubAPI = GitHubAPI()) {
+        self.gitHubAPI = gitHubAPI
     }
 
     public func getSearchResults(_ term: String) -> AnyPublisher<SearchResponse, Error> {
-        let url = URL(string: "https://api.github.com/search/repositories?q=\(term)")!
-        let publisher = network.sendRequest(url: url)
-            .tryMap { (data) -> SearchResponse in
-                try JSONDecoder().decode(SearchResponse.self, from: data)
-            }.eraseToAnyPublisher()
-        return publisher
+        return gitHubAPI.getSearchResults(term)
+            .map({ (response) -> SearchResponse in
+                return SearchResponse(total_count: response.total_count)
+            }).eraseToAnyPublisher()
     }
 }
