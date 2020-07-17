706b86da42dc0e3e82f01b15f2bae7dc2ad2b0d8
Shagun Madhikarmi <madhikarma@gmail.com>
2020-07-18 00:39:21 +0100

    CoreDataDemo: New demo project from Xcode template

---
+++ core-data-demo-2/CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Shagun Madhikarmi on 18/07/2020.
+//  Copyright © 2020 madhikarma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
